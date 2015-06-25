36952d4dafe214b7c0139680c8821db391f11e12
Marco Pozzato <ironpotts@gmail.com>
2015-06-25 16:49:00 +0200

    consistent language navigation

--- public/plugin/function.lang.php
+++ public/plugin/function.lang.php
@@ -14,6 +14,27 @@ function smarty_function_lang($params, &$smarty){
   $session_lang = getLangId();
   $session_lang_initial = getLangInitial();
 
+  $server    = 'http://'.$_SERVER['SERVER_NAME'];
+  $page_id   = $smarty->getTemplateVars( 'synPageId' );
+  $alternate = $smarty->getTemplateVars( 'alternate' );
+  $item      = $smarty->getTemplateVars( 'item' );
+  $visible   = explode('|', $smarty->getTemplateVars( 'synPageVisible' ));
+  $alt_uris  = array();
+
+  if ( empty( $alternate ) && empty( $item )) {
+    foreach( $visible as $lang_visible ){
+      // for each language different from the selected one, provide an alternate href
+      if ( in_array($session_lang, $visible) ) {
+        $initial  = $languages['list'][$lang_visible];
+        $alt_uris[ $initial ] = $server.createPath( $page_id, $initial );
+      }
+    }
+  } else {
+    // it's an item and there are alternate
+    foreach( $alternate as $alt_lang => $alt_link )
+      $alt_uris[ $alt_lang ] = $alt_link;
+  }
+
   $active_lang = null;
   $list = array();
 
@@ -31,9 +52,15 @@ function smarty_function_lang($params, &$smarty){
     } else {
       $selected = false;
     }
-    $path = ($default == '1')
-          ? '/'
-          : '/'.$initial.'/';
+
+    if ( isset($alt_uris[ $initial ]) ) {
+      // recupero l'uri dagli alternate uris
+      $path = $alt_uris[ $initial ];
+    } else {
+      // se non è visibile in questa lingua, rimando alla home
+      $path = '/' . ($default == '1') ? NULL : $initial.'/';
+    }
+
     $list[] = array(
       'path'    => $path,
       'name'    => $lang,
